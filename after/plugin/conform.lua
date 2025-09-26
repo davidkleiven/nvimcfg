fc8125725859139acf9f9e72b357c7a6b29b7357
David Kleiven <david.kleiven@statnett.no>
2025-09-26 08:16:15 +0200

    feat: run ruff check and ruff format

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,22 +1,29 @@
 require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "ruff_format", "isort", "black" },
-    rust = { "rustfmt", lsp_format = "fallback" },
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-    go = { "gofmt" },
-  },
-  formatters = {
-    ruff_format = {
-      command = "uvx",
-      args = { "ruff", "format", "--stdin-filename", "$FILENAME", "-" },
-      stdin = true,
-    },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "ruff_format" },
+		rust = { "rustfmt", lsp_format = "fallback" },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		go = { "gofmt" },
+	},
+	formatters = {
+		ruff_format = {
+			command = "bash",
+			args = {
+				"-c",
+				[[
+			uvx ruff check --fix --stdin-filename $1 - && uvx ruff format -
+			]],
+				"--",
+				"$FILENAME",
+			},
+			stdin = true,
+		},
+	},
 })
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
