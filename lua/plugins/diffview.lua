b38482d07a86f029712bcec47339295e4430c3e4
David Kleiven <david.kleiven@statnett.no>
2025-10-02 07:55:16 +0200

    Upgrade to tree-sitter main branch. Require tree-sitter cli

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,9 +1,14 @@
-require("lazy").setup({
+return {
 	{
 		"sindrets/diffview.nvim",
 		dependencies = "nvim-lua/plenary.nvim",
 		config = function()
 			require("diffview").setup()
+
+			--- Key bindings ---
+			vim.keymap.set("n", "<leader>dv", ":DiffviewOpen<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>dc", ":DiffviewClose<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>dh", ":DiffviewFileHistory<CR>", { silent = true })
 		end,
 	},
-})
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,1 +1,16 @@
-return  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		ts = require("nvim-treesitter")
+		ts.install({ "go", "python", "kotlin", "markdown", "javascript" })
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "go", "python", "kotlin", "markdown" },
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
+	end,
+}
