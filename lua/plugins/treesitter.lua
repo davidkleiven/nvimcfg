53b08f0188e336a8a8626ce3d83be9bc04932ab7
David Kleiven <david.kleiven@statnett.no>
2025-10-09 08:11:46 +0200

    feat: add turtle to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,9 +5,9 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		ts = require("nvim-treesitter")
-		ts.install({ "go", "python", "kotlin", "markdown", "javascript" })
+		ts.install({ "go", "python", "kotlin", "markdown", "javascript", "turtle" })
 		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "go", "python", "kotlin", "markdown" },
+			pattern = { "go", "python", "kotlin", "markdown", "turtle" },
 			callback = function()
 				vim.treesitter.start()
 			end,
