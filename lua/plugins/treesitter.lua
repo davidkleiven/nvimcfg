3ee642075ea107e7a0783d06e9738f33b734358f
David Kleiven <davidkleiven446@gmail.com>
2025-11-09 16:31:49 +0100

    feat: include yaml in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,9 +5,9 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		ts = require("nvim-treesitter")
-		ts.install({ "go", "python", "kotlin", "markdown", "javascript", "turtle" })
+		ts.install({ "go", "python", "kotlin", "markdown", "javascript", "turtle", "yaml" })
 		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "go", "python", "kotlin", "markdown", "turtle" },
+			pattern = { "go", "python", "kotlin", "markdown", "turtle", "yaml" },
 			callback = function()
 				vim.treesitter.start()
 			end,
