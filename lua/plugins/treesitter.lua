3981b2d78c4ce114debba1af752ea242b0075206
David Kleiven <davidkleiven446@gmail.com>
2026-04-08 06:55:49 +0200

    feat: add templ to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,9 +5,21 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		ts = require("nvim-treesitter")
-		ts.install({ "go", "python", "kotlin", "markdown", "javascript", "turtle", "yaml", "typescript", "tsx", "latex" })
+		ts.install({
+			"go",
+			"javascript",
+			"kotlin",
+			"latex",
+			"markdown",
+			"python",
+			"templ",
+			"tsx",
+			"turtle",
+			"typescript",
+			"yaml",
+		})
 		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "go", "python", "kotlin", "markdown", "turtle", "yaml", "typescript", "tsx", "tex" },
+			pattern = { "go", "python", "kotlin", "markdown", "turtle", "yaml", "typescript", "tsx", "tex", "templ" },
 			callback = function()
 				vim.treesitter.start()
 			end,
