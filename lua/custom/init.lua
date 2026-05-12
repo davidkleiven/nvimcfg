6afd4bab8b781885ad4dcbc71077909b10d4f7f2
David Kleiven <davidkleiven446@gmail.com>
2026-05-12 07:15:23 +0200

    feat: add LspRestart command

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,8 +1,18 @@
 require("custom.remap")
-print("Hello from custom")
 vim.filetype.add({
 	extension = {
 		tf = "terraform",
 		tfvars = "terraform",
 	},
 })
+
+vim.api.nvim_create_user_command("LspRestart", function()
+	local clients = vim.lsp.get_clients()
+	for _, client in ipairs(clients) do
+		client:stop()
+	end
+
+	vim.defer_fn(function()
+		vim.cmd("edit")
+	end, 100)
+end, {})
