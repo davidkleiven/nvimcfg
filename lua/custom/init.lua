4213095b26af897f69e28a45c27324041905585b
David Kleiven <davidkleiven446@gmail.com>
2026-06-01 15:21:12 +0200

    feat: add command for switching to light mode

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -16,3 +16,20 @@ vim.api.nvim_create_user_command("LspRestart", function()
 		vim.cmd("edit")
 	end, 100)
 end, {})
+
+vim.api.nvim_create_user_command("RosePine", function(opts)
+	local variant = opts.args
+	if variant == "" then
+		print("Usage: RosePine main | moon | dawn")
+		return
+	end
+
+	vim.g.rose_pine_variant = variant
+	vim.opt.background = (variant == "dawn") and "light" or "dark"
+	vim.cmd("colorscheme rose-pine")
+end, {
+	nargs = 1,
+	complete = function()
+		return { "main", "moon", "dark" }
+	end,
+})
